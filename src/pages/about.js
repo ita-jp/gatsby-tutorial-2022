07b38d2172f8f378cbf48fb8cfcaf222eadd0cb2
ita-jp <tn00gm@gmail.com>
2022-06-28 21:51:48 +0900

    Use the <Link> component
    https://www.gatsbyjs.com/docs/tutorial/part-2/#use-the-link-component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 
 const AboutPage = () => {
@@ -5,6 +6,7 @@ const AboutPage = () => {
         <main>
             <title>About Me</title>
             <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
         </main>
     )
