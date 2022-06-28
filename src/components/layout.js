c266203b89b8e9fcbc1dbca4c014180eb67e32de
ita-jp <tn00gm@gmail.com>
2022-06-28 22:00:59 +0900

    Create a reusable layout component
    https://www.gatsbyjs.com/docs/tutorial/part-2/#create-a-reusable-layout-component

---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+import { Link } from 'gatsby'
+import * as React from 'react'
+
+const Layout = ({ pageTitle, children}) => {
+    return (
+        <div>
+          <title>{pageTitle}</title>
+          <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+          <main>
+            <h1>{pageTitle}</h1>
+            {children}
+          </main>
+        </div>
+    )
+}
+
+export default Layout
